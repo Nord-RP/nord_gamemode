31fae8c3a98175972a2fd4fcc581469d5965b5ce
Allerek <allerek22@interia.pl>
2023-03-09 17:31:48 +0100

    Login Panel BG video+music+buttons

--- [MAIN]/rp_login/c_gui_data.lua
+++ [MAIN]/rp_login/c_gui_data.lua
@@ -1,26 +1,38 @@
 local sW, sH = guiGetScreenSize()
-local zoom = sW/1920
+zoom = sW/1920
 
 GUI = {}
 GUI.txt = {
     ["panel_bg"] = dxCreateTexture("files/img/panel_bg.png", "argb", true, "wrap"),
     ["panel_bg_left"] = dxCreateTexture("files/img/panel_bg_left.png", "argb", true, "wrap"),
     ["online"] = dxCreateTexture("files/img/online.png", "argb", true, "wrap"),
-    ["online_blink"] = dxCreateTexture("files/img/online_blink.png", "argb", true, "wrap")
+    ["online_blink"] = dxCreateTexture("files/img/online_blink.png", "argb", true, "wrap"),
+    --Login
+    ["login"] = dxCreateTexture("files/img/login.png", "argb", true, "wrap"),
+    ["login_hover"] = dxCreateTexture("files/img/login_hover.png", "argb", true, "wrap"),
+    ["login_off"] = dxCreateTexture("files/img/login_off.png", "argb", true, "wrap"),
+    --Register
+    ["register"] = dxCreateTexture("files/img/register.png", "argb", true, "wrap"),
+    ["register_hover"] = dxCreateTexture("files/img/register_hover.png", "argb", true, "wrap"),
+    ["register_off"] = dxCreateTexture("files/img/register_off.png", "argb", true, "wrap")
 }
 
 GUI.scale = {
     ["panel_bg"] = {w=1395/zoom, h=947/zoom},
     ["panel_bg_left"] = {w=538/zoom, h=967/zoom},
-    ["online"] = {w=32/zoom, h=32/zoom}
+    ["online"] = {w=32/zoom, h=32/zoom},
+    ["login"] = {w=182/zoom, h=45/zoom},
+    ["register"] = {w=189/zoom, h=45/zoom}
 }
 
 GUI.pos = {
     ["panel_bg"] = {x=sW/2-GUI.scale.panel_bg.w/2, y = sH/2-GUI.scale.panel_bg.h/2},
     ["panel_bg_left"] = {x=0,y=GUI.scale.panel_bg.h/2-GUI.scale.panel_bg_left.h/2},
-    ["online_label"] = {x=GUI.scale.panel_bg.w-96/zoom,y=138/zoom},
-    ["counter_label"] = {x=GUI.scale.panel_bg.w-96/zoom,y=80/zoom},
-    ["online"] = {x=GUI.scale.panel_bg.w-180/zoom,y=139/zoom}
+    ["counter_label"] = {x=GUI.scale.panel_bg.w-139/zoom,y=120/zoom},
+    ["online_label"] = {x=GUI.scale.panel_bg.w-139/zoom,y=178/zoom},
+    ["online"] = {x=GUI.scale.panel_bg.w-224/zoom,y=180/zoom},
+    ["login"] = {x=565/zoom,y=285/zoom},
+    ["register"] = {x=772/zoom,y=285/zoom}
 }
 
 GUI.fonts = {
--- [MAIN]/rp_login/c_rp_login.lua
+++ [MAIN]/rp_login/c_rp_login.lua
@@ -1,20 +1,40 @@
 loadstring(exports.dgs:dgsImportOOPClass(true))()-- load OOP class
-
-
+local sW,sH = guiGetScreenSize()
+local dev = true
 
 addEventHandler("onClientResourceStart", resourceRoot, function()
-    print("Start");
     showChat(false);
+    showCursor(true);
     setPlayerHudComponentVisible("all", false);
 
+    if(not dev) then
+        GUI.elements.sound = Sound("files/sound/bg_music.mp3");
+        GUI.elements.sound:setVolume(0.5);
+    end
+    GUI.elements.browser = dgsCreateMediaBrowser(sW,sH);
+    dgsMediaLoadMedia(GUI.elements.browser,"files/movie/bg_video.webm", "video")
+    GUI.elements.browser_image = dgsImage(0,0,sW,sH, GUI.elements.browser, false);
+    dgsMediaPlay(GUI.elements.browser);
+
     GUI.elements.bg = dgsImage(GUI.pos.panel_bg.x,GUI.pos.panel_bg.y, GUI.scale.panel_bg.w, GUI.scale.panel_bg.h, GUI.txt.panel_bg);
+    GUI.elements.bg:setParent(GUI.elements.browser_image);
+
     GUI.elements.bg_left = dgsImage(GUI.pos.panel_bg_left.x,GUI.pos.panel_bg_left.y, GUI.scale.panel_bg_left.w, GUI.scale.panel_bg_left.h, GUI.txt.panel_bg_left);
     GUI.elements.bg_left:setParent(GUI.elements.bg);
+
+    GUI.elements.login_tab = dgsButton(GUI.pos.login.x-10/zoom, GUI.pos.login.y, GUI.scale.login.w, GUI.scale.login.h, "", false, nil, tocolor(255,255,255), 1, GUI.txt.login, GUI.txt.login_hover, GUI.txt.login, tocolor(255,255,255),tocolor(255,255,255),tocolor(255,255,255))
+    GUI.elements.login_tab:setParent(GUI.elements.bg);
+
+    GUI.elements.register_tab = dgsButton(GUI.pos.register.x, GUI.pos.register.y, GUI.scale.register.w, GUI.scale.register.h, "", false, nil, tocolor(255,255,255), 1, GUI.txt.register_off, GUI.txt.register_hover, GUI.txt.register_off, tocolor(255,255,255),tocolor(255,255,255),tocolor(255,255,255))
+    GUI.elements.register_tab:setParent(GUI.elements.bg);
+
+    GUI.vars.tab = "login";
+
     GUI.elements.online_label = dgsLabel(GUI.pos.online_label.x, GUI.pos.online_label.y, 0, 0, "Online", false, tocolor(255,255,255), 1, 1, nil, nil, nil, "right" , "top")
     GUI.elements.online_label:setParent(GUI.elements.bg);
     GUI.elements.online_label:setFont(GUI.fonts.font_4);
 
-    GUI.elements.counter_label = dgsLabel(GUI.pos.counter_label.x, GUI.pos.counter_label.y, 0, 0, "194", false, tocolor(255,255,255), 1, 1, nil, nil, nil, "right" , "top")
+    GUI.elements.counter_label = dgsLabel(GUI.pos.counter_label.x, GUI.pos.counter_label.y, 0, 0, #getElementsByType("player"), false, tocolor(255,255,255), 1, 1, nil, nil, nil, "right" , "top")
     GUI.elements.counter_label:setParent(GUI.elements.bg);
     GUI.elements.counter_label:setFont(GUI.fonts.font_12);
 
@@ -33,5 +53,64 @@ addEventHandler("onClientResourceStart", resourceRoot, function()
             GUI.elements.online_blink:alphaTo(0, "Linear", 500); 
             GUI.vars.online = false
         end
+        GUI.elements.counter_label:setProperty("text", #getElementsByType("player"));
     end, 1000, 0);
+
+    addEventHandler("onDgsMouseEnter", root, dgsEnter)
+    addEventHandler("onDgsMouseLeave", root, dgsLeave)
+    addEventHandler("onDgsMouseClick", root, dgsClick)
+    --addEventHandler("onClientRender", getRootElement(), renderSound)
 end)
+
+function dgsClick()
+    if(source == GUI.elements.register_tab:getElement()) then
+        if not (GUI.vars.tab == "register") then
+            GUI.vars.tab = "register"
+            GUI.elements.login_tab:moveTo(GUI.pos.login.x, GUI.pos.login.y, false, "Linear", 100);
+            GUI.elements.register_tab:setProperty("image", {GUI.txt.register, GUI.txt.register_hover, GUI.txt.register})
+            GUI.elements.login_tab:setProperty("image", {GUI.txt.login_off, GUI.txt.login_hover, GUI.txt.login_off})
+        end
+    elseif(source == GUI.elements.login_tab:getElement()) then
+        if not (GUI.vars.tab == "login") then
+            GUI.vars.tab = "login"
+            GUI.elements.register_tab:moveTo(GUI.pos.register.x, GUI.pos.register.y, false, "Linear", 100);
+            GUI.elements.register_tab:setProperty("image", {GUI.txt.register_off, GUI.txt.register_hover, GUI.txt.register_off})
+            GUI.elements.login_tab:setProperty("image", {GUI.txt.login, GUI.txt.login_hover, GUI.txt.login})
+        end
+    end
+end
+
+function dgsEnter()
+    if(source == GUI.elements.register_tab:getElement()) then
+        if not (GUI.vars.tab == "register") then
+            GUI.elements.register_tab:moveTo(GUI.pos.register.x+10/zoom, GUI.pos.register.y, false, "Linear", 100);
+        end
+    elseif(source == GUI.elements.login_tab:getElement()) then
+        if not (GUI.vars.tab == "login") then
+            GUI.elements.login_tab:moveTo(GUI.pos.login.x-10/zoom, GUI.pos.login.y, false, "Linear", 100);
+        end
+    end
+end
+
+function dgsLeave()
+    if(source == GUI.elements.register_tab:getElement()) then
+        if not (GUI.vars.tab == "register") then
+            GUI.elements.register_tab:moveTo(GUI.pos.register.x, GUI.pos.register.y, false, "Linear", 100);
+        end
+    elseif(source == GUI.elements.login_tab:getElement()) then
+        if not (GUI.vars.tab == "login") then
+            GUI.elements.login_tab:moveTo(GUI.pos.login.x, GUI.pos.login.y, false, "Linear", 100);
+        end
+    end
+end
+
+function renderSound()
+    if ( GUI.elements.sound ) then
+        local soundFFT = GUI.elements.sound:getFFTData (2048, 256 )
+	if ( soundFFT ) then
+            for i = 0, 4 do -- Data starts from index 0
+                dxDrawRectangle ( i*20, sH, 20, -(math.sqrt ( soundFFT[i] ) * 100) )
+            end
+        end
+    end
+end
